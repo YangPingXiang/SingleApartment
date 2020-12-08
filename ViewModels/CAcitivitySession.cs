47213cc84d4eaf8246e652b3d47db1f22c0bb08e
Louislien <x5994777@yahoo.com.tw>
2020-12-08 12:01:58 +0800

    活動更新
    活動更新

---
+++ ViewModels/CAcitivitySession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sln_SingleApartment.ViewModels
+{
+    public class CAcitivitySession
+    {
+        public int txtActivityID { get; set; }
+    }
+}
