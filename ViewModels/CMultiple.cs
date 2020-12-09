7c21e4d97184e8ec69be36dc9c76945adb63c309
Paul0801 <Paul0801@users.noreply.github.com>
2020-12-09 15:32:12 +0800

    (12/09)新增會員中心-于
    顯示會員中心

---
+++ ViewModels/CMultiple.cs
@@ -0,0 +1,17 @@
+﻿using sln_SingleApartment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sln_SingleApartment.ViewModels
+{
+    public class CMultiple
+    {
+        public tMember t { get; set; }
+        public List<Activity> atv { get; set; }
+        public List<Product> prod { get; set; }
+        public Room r { get; set; }
+        public RoomStyle rs {get;set;}
+    }
+}
